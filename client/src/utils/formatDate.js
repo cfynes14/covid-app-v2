e4b5ea77c8a4ac11ead0a69ca3c78430fc92d42d
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-08-04 15:17:52 +0100

    add client side

--- client/src/utils/formatDate.js
+++
@@ -1,8 +0,0 @@
-function formatDate(dateString)
-{
-    let allDate = dateString.split(' ');
-    let thisDate = allDate[0].split('-');
-    let newDate = [thisDate[2],thisDate[1],thisDate[0] ].join("-");
-    return newDate;
-}
-export default formatDate
