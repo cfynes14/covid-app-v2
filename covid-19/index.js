9654ad16bffa2d0ddf294635bba6f0eb631207ee
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-08-04 14:39:38 +0100

    add condition for production

--- covid-19/index.js
+++ covid-19/index.js
@@ -16,6 +16,13 @@ app.use(cors({
   credentials: true
 }))
 
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
