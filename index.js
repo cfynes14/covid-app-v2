8b1cbee4563bcf0543419c9f13024dfe41727e20
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-08-04 15:04:35 +0100

    server React from express

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ app.use(cors({
   credentials: true
 }))
 
-// if (process.env.NODE_ENV === "production"){
-//   app.use(express.static("build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-//   });
-// }
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
