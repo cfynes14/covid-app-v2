71a501e1d0f5d4b181f3c508629434e922b6252e
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2022-09-09 08:34:25 +0100

    update server path to ensure client folder is linked

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const localAuthorityData = require('./server/utils/localAuthorityData');
 
 app.use(cors())
   
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
